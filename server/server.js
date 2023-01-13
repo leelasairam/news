a1de568bfe39c30b7c171ab89b6170ef693b2673
leelasairam <saikodavali0948@gmail.com>
2023-01-13 21:46:44 +0530

    added port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 8000
+const port = process.env.PORT || 8000
 const NewsAPI = require('newsapi');
 const { response } = require('express');
 require('dotenv').config();
